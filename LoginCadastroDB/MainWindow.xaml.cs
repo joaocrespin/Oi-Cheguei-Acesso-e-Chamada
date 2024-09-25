e5b803d31e55384dd607eae6388e6d260c3949c4
João Crespin <joaorato58@gmail.com>
2024-09-25 20:15:17 -0300

    Tabela criada, código não salva na tabela

--- LoginCadastroDB/MainWindow.xaml.cs
+++ LoginCadastroDB/MainWindow.xaml.cs
@@ -18,9 +18,44 @@ namespace LoginCadastroDB
     /// </summary>
     public partial class MainWindow : Window
     {
+        private SQLiteConnection _connection;
         public MainWindow()
         {
             InitializeComponent();
+            Conexao();
+        }
+
+        private void Conexao()
+        {
+            _connection = new SQLiteConnection("Data Source=EscolaDB.db; Version=3;");
+            _connection.Open();
+        }
+
+        private void botaoCadastrar_Click(object sender, RoutedEventArgs e)
+        {
+            string usuario = caixaNome.Text;
+            string senha = caixaSenha.Password; 
+            string cpf = caixaCpf.Text;
+            string cargo = caixaCargo.Text;
+            string contato = caixaContato.Text;
+
+            string inserirDados = "INSERT INTO Usuarios (Nome, Senha, CPF, Cargo, Contato) VALUES(@nome, @senha, @cpf, @cargo, @contato)";
+            SQLiteCommand mandaDados = new SQLiteCommand(inserirDados, _connection);
+            mandaDados.Parameters.AddWithValue("@nome", usuario);
+            mandaDados.Parameters.AddWithValue("@senha", senha);
+            mandaDados.Parameters.AddWithValue("@cpf", cpf);
+            mandaDados.Parameters.AddWithValue("@cargo", cargo);
+            mandaDados.Parameters.AddWithValue("@contato", contato);
+
+            try
+            {
+                mandaDados.ExecuteNonQuery();
+                MessageBox.Show("Dados salvos com sucesso!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro: " + ex.Message);
+            }
         }
     }
 }
